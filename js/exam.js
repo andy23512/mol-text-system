15431eac049852dffffd992d8a4ac94c5c103994
Tangent Chang <andy23512@gmail.com>
2014-04-26 00:41:08 +0800

    Add scoring

--- js/exam.js
+++ js/exam.js
@@ -1,11 +1,26 @@
 (function($,undefined){
 	$(document).ready(function(){
+		var q;
 		$("#start").click(function(){
 			$.getJSON('php/exam.php',function(d){
+				q=d;
 				d.forEach(function(p,i){
 					$('#exam').append('<div class="ques">題目:'+p.ques+'<p class="ques"></p>答案:<input class="ans"/></div>');
 				});
 			});	
 		});
+		var total = $('.ans').length;
+		$('#submit').click(function(){
+			var correct = 0;
+			total = $('.ans').length;
+			$('.ans').each(function(i){
+				if(q[i].ans== $(this).val())++correct;
+				if(i==total-1){
+					score = 100*correct/total;
+					$('#result').text('對 '+correct+' 題, 總分 '+score+' 分');
+				}
+			});
+		});
+		
 	});
 })(jQuery);
